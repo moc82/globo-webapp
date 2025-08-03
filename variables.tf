7d4a9ec471e73f5e63191eed10939ce5b0b6a7e1
carloym <26226634+moc82@users.noreply.github.com>
2025-08-03 18:11:10 +0800

    fix:Updated the region

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "region" {
   type        = string
   description = "(Optional) AWS Region to deploy in. Defaults to us-east-1."
-  default     = "us-east-1"
+  default     = "ap-southeast-1"
 }
 
 variable "prefix" {
